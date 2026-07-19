8d34dfd1f5c9e56a0129f5bf8b5a6273eb51861f
Leif <leif.algo@pm.me>
2026-07-19 12:39:21 -0600

    fix: restore recipientPublicKey argument in sendReply encryptReply call

--- src/services/algorand.service.ts
+++ src/services/algorand.service.ts
@@ -206,6 +206,7 @@ export class AlgorandService {
             replyToTxid,
             replyToPreview,
             chatAccount.encryptionKeys.publicKey,
+            recipientPublicKey,
             this.encryptionOptions
         );
 
